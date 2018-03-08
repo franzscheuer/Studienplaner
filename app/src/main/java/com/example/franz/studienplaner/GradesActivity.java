c08efd5754e1c8b5c515b518ef1a8f70b8928b89
franzscheuer <Franz.Scheuer@stud.uni-regensburg.de>
2018-03-08 17:07:14 +0100

    Spinner
    - alle Spinner belegt

--- app/src/main/java/com/example/franz/studienplaner/GradesActivity.java
+++ app/src/main/java/com/example/franz/studienplaner/GradesActivity.java
@@ -3,10 +3,12 @@ package com.example.franz.studienplaner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class GradesActivity extends AppCompatActivity implements View.OnClickListener {
+public class GradesActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     Spinner spinner1, spinner2;
     TextView grades;
@@ -18,16 +20,84 @@ public class GradesActivity extends AppCompatActivity implements View.OnClickLis
         getSupportActionBar().setTitle("Notenübersicht");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setupUI();
+        disableSecondSpinner();
+        fillFirstSpinner();
     }
 
     private void setupUI() {
         spinner1 = findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(this);
         spinner2 = findViewById(R.id.spinner2);
+        //spinner2.setOnItemSelectedListener(this);
         grades = findViewById(R.id.textView);
+    }
+
+    private void disableSecondSpinner() {
+        spinner2.setEnabled(false);
+    }
+
+    private void enableSecondSpinner() {
+        spinner2.setEnabled(true);
+    }
+
+    private void fillFirstSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.noten_auswahl, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+    }
+
+    private void fillSecondSpinner(String k) {
+        if(k.equals("mei")) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.mei_module, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter);
+        } else if(k.equals("inf")) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.inf_module, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter);
+        }
+    }
+
+    private void emptySecondSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.empty_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter);
+    }
+
+    private void fillTextView(String g) {
+
     }
 
     @Override
     public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        switch(adapterView.getItemAtPosition(i).toString()){
+            case "Gesamtübersicht":
+                emptySecondSpinner();
+                disableSecondSpinner();
+                //fillTextView("all");
+                break;
+            case "Medieninformatik":
+                enableSecondSpinner();
+                fillSecondSpinner("mei");
+                break;
+            case "Informationswissenschaften":
+                enableSecondSpinner();
+                fillSecondSpinner("inf");
+                break;
+            default:
+                emptySecondSpinner();
+                disableSecondSpinner();
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
--- app/src/main/java/com/example/franz/studienplaner/SubjectsActivity.java
+++ app/src/main/java/com/example/franz/studienplaner/SubjectsActivity.java
@@ -3,12 +3,14 @@ package com.example.franz.studienplaner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class SubjectsActivity extends AppCompatActivity implements View.OnClickListener {
+public class SubjectsActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     Spinner spinner;
     TextView position1, position2, position3;
@@ -23,14 +25,16 @@ public class SubjectsActivity extends AppCompatActivity implements View.OnClickL
         getSupportActionBar().setTitle(getSubject());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setupUI();
+        hideAll();
+        fillSpinner(getSubject());
     }
 
     private String getSubject() {
         String subject = "";
         Bundle extras = getIntent().getExtras();
-        if(extras.getInt("subject") == 0x7f070026) {
+        if(extras.getInt("subject") == 0x7f080026) {
             subject = "Medieninformatik";
-        } else if(extras.getInt("subject") == 0x7f070025) {
+        } else if(extras.getInt("subject") == 0x7f080025) {
             subject = "Informationswissenschaften";
         }
         return subject;
@@ -38,6 +42,7 @@ public class SubjectsActivity extends AppCompatActivity implements View.OnClickL
 
     private void setupUI() {
         spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
         position1 = findViewById(R.id.position1);
         position2 = findViewById(R.id.position2);
         position3 = findViewById(R.id.position3);
@@ -52,8 +57,85 @@ public class SubjectsActivity extends AppCompatActivity implements View.OnClickL
         note3 = findViewById(R.id.note3);
     }
 
+    private void fillSpinner(String i) {
+        if(i.equals("Medieninformatik")) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.mei_module, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        } else if(i.equals("Informationswissenschaften")) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.inf_module, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    private void showAll() {
+        position1.setVisibility(View.VISIBLE);
+        position2.setVisibility(View.VISIBLE);
+        position3.setVisibility(View.VISIBLE);
+        checkbox1.setVisibility(View.VISIBLE);
+        checkbox2.setVisibility(View.VISIBLE);
+        checkbox3.setVisibility(View.VISIBLE);
+        name1.setVisibility(View.VISIBLE);
+        name2.setVisibility(View.VISIBLE);
+        name3.setVisibility(View.VISIBLE);
+        note1.setVisibility(View.VISIBLE);
+        note2.setVisibility(View.VISIBLE);
+        note3.setVisibility(View.VISIBLE);
+    }
+
+    private void hideAll() {
+        position1.setVisibility(View.INVISIBLE);
+        position2.setVisibility(View.INVISIBLE);
+        position3.setVisibility(View.INVISIBLE);
+        checkbox1.setVisibility(View.INVISIBLE);
+        checkbox2.setVisibility(View.INVISIBLE);
+        checkbox3.setVisibility(View.INVISIBLE);
+        name1.setVisibility(View.INVISIBLE);
+        name2.setVisibility(View.INVISIBLE);
+        name3.setVisibility(View.INVISIBLE);
+        note1.setVisibility(View.INVISIBLE);
+        note2.setVisibility(View.INVISIBLE);
+        note3.setVisibility(View.INVISIBLE);
+    }
+
+    private void hideLast() {
+        position3.setVisibility(View.INVISIBLE);
+        checkbox3.setVisibility(View.INVISIBLE);
+        name3.setVisibility(View.INVISIBLE);
+        note3.setVisibility(View.INVISIBLE);
+    }
+
+    private void fillInformation(String k) {
+
+    }
+
     @Override
     public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        switch(adapterView.getItemAtPosition(i).toString()){
+            case "MEI-M01":
+            case "MEI-M02":
+            case "INF-M01":
+            case "INF-M05":
+            case "INF-M07":
+                showAll();
+                hideLast();
+                //fillInformation(adapterView.getItemAtPosition(i).toString());
+                break;
+            default:
+                showAll();
+                //fillInformation(adapterView.getItemAtPosition(i).toString());
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
